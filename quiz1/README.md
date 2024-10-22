4debdcd8ac860d0e9bd594d443d990b323288b24
jcg517 <jessegabriel517@gmail.com>
2024-10-22 15:13:25 -0400

    quiz1

---
+++ quiz1/README.md
@@ -0,0 +1,10 @@
+# VM LINK: 
+gabrij2.eastus.cloudapp.azure.com/quiz1
+
+
+In order to combined this quiz work with my previous lab work with the weather and quotes api, I first copied my lab 2 code and worked on including all quiz functionality in the quiz1 function in the javascript. 
+
+
+# Sources
+Websys lab 2, 3 and 4 code.
+Frankfurt api and documentation https://api.frankfurter.app/latest
---
+++ quiz1/store.php
@@ -0,0 +1,39 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "gabrij2";
+$password = "dixie2";
+$dbname = "quiz1";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Get JSON
+$json_data = file_get_contents('php://input');
+$data = json_decode($json_data, true);
+
+
+if (!isset($data['name']) || !isset($data['data'])) {
+    echo json_encode(['success' => false, 'message' => 'Invalid data provided']);
+    exit();
+}
+
+$stmt = $conn->prepare("INSERT INTO quiz1 data) VALUES (?)");
+$stmt->bind_param("s", $data);
+
+
+$data_serialized = json_encode($data['data']);
+
+if ($stmt->execute()) {
+    echo json_encode(['success' => true, 'message' => 'success']);
+} else {
+    echo json_encode(['success' => false, 'message' => 'failed']);
+}
+
+$stmt->close();
+$conn->close();
+?>
