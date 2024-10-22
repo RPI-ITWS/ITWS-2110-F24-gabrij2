9aca73d9845bcf4e620c26a47706219a9a4a1fe9
jcg517 <jessegabriel517@gmail.com>
2024-10-22 15:35:18 -0400

    quiz1

--- quiz1/store.php
+++ quiz1/store.php
@@ -11,20 +11,12 @@ $dbname = "quiz1";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
 // Get JSON
 $json_data = file_get_contents('php://input');
 $data = json_decode($json_data, true);
 
-
-if (!isset($data['name']) || !isset($data['data'])) {
-    echo json_encode(['success' => false, 'message' => 'Invalid data provided']);
-    exit();
-}
-
 $stmt = $conn->prepare("INSERT INTO quiz1 data) VALUES (?)");
 $stmt->bind_param("s", $data);
-
 
 $data_serialized = json_encode($data['data']);
 
